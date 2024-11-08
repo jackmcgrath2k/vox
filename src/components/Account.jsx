4384b391c183da2e65bef268216bf412e594f2da
Jack <jackmcgrath2k@gmail.com>
2024-11-08 20:55:24 +0000

    Account interface implemented

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -24,9 +24,23 @@ import {
   TabsList, 
   TabsTrigger
 } from "@/components/ui/tabs"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { Label } from "@/components/ui/label"
 import { Button } from "./ui/button";
 import { Gem } from 'lucide-react';
+import { Input } from "@/components/ui/input"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Separator } from "@/components/ui/separator"
+import { Switch } from "@/components/ui/switch"
+
 
 
 
@@ -45,14 +59,21 @@ export default function Account() {
         <TabsTrigger value="account">Account</TabsTrigger>
         <TabsTrigger value="password">Edit</TabsTrigger>
       </TabsList>
-      <TabsContent value="account">
+      <TabsContent value="account" className="justify-center text-center">
         <Card>
           <CardHeader>
-            <CardTitle>Jack</CardTitle>
+          <div className="flex flex-col items-center gap-4">
+            <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>J</AvatarFallback>
+            </Avatar>
+            <CardTitle className="font-bold text-4xl">Jack</CardTitle>
+            </div>
             <CardDescription>
               Member since: Jan 2025
             </CardDescription>
           </CardHeader>
+          <Separator orientation="horizontal" className="bg-gray-300 mb-5" />
           <CardContent className="space-y-2">
             <div className="space-y-1">
               <Label>Total Entries</Label>
@@ -60,18 +81,17 @@ export default function Account() {
             </div>
             <div className="space-y-1">
               <Label>Account Type:</Label>
-              <div className="flex m-1">
+              <div className="text-gray-500">
               <h1>Free Tier</h1>
-              
               </div>
             </div>
           </CardContent>
-          <CardFooter>
-            <div>
-            <Button className="border border-lg hover:bg-gradient-to-r from-amber-300 via-yellow-400 to-amber-200 hover:text-black">
+          <Separator orientation="horizontal" className="bg-gray-300" />
+          <CardFooter className="flex justify-center">
+            <div className="my-5">
+            <Button className="bg-gradient-to-r from-amber-500 via-yellow-600 to-amber-400 hover:text-black">
             <Gem />Upgrade
             </Button>
-            
             </div>
           </CardFooter>
         </Card>
@@ -81,25 +101,150 @@ export default function Account() {
           <CardHeader>
             <CardTitle>Edit Account</CardTitle>
             <CardDescription>
-              Change your password here. After saving, you'll be logged out.
+               After saving, you'll be logged out.
             </CardDescription>
           </CardHeader>
+          
           <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="current">Current password</Label>
-              <h1>hi</h1>
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">New password</Label>
-              <h1>hi</h1>
+            <div className="flex flex-col space-y-2">
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button>Change details</Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Edit profile</DialogTitle>
+                  <DialogDescription>
+                    Make changes to your profile here. Click save when you're done.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Name
+                    </Label>
+                    <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="username" className="text-right">
+                      Photo
+                    </Label>
+                    <Input id="username" value="@peduarte" className="col-span-3" />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <Button type="submit">Save changes</Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button>Change password</Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Change password</DialogTitle>
+                  <DialogDescription>
+                    Make changes to your profile here. Click save when you're done.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Current password
+                    </Label>
+                    <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="username" className="text-right">
+                      New password
+                    </Label>
+                    <Input id="username" value="@peduarte" className="col-span-3" />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <Button>ayo i forgot bruh</Button>
+                  <Button type="submit">Save changes</Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button>Subscription settings</Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Subscription settings</DialogTitle>
+                  <DialogDescription>
+                    Change or exit your plan.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Current password
+                    </Label>
+                    <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="username" className="text-right">
+                      New password
+                    </Label>
+                    <Input id="username" value="@peduarte" className="col-span-3" />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <Button type="submit">Save changes</Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button>Notifcation settings</Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Notification settings</DialogTitle>
+                  <DialogDescription>
+                    Adjust your preferences for notifications.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Notifications
+                    </Label>
+                    <Switch />
+                    <p className="text-xs text-gray-300">*Toggle notifications on or off</p>
+                  </div>
+                </div>
+                <DialogFooter>
+                  <p>This can be adjusted anytime.</p>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button>Contact Support</Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Contact Support</DialogTitle>
+                  <DialogDescription>
+                    email type thing
+                  </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                  <p>put em here or somehting</p>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
             </div>
           </CardContent>
-          <CardFooter>
-            <div className="flex">
-            
-            <AlertDialog>
+          <div className="flex justify-center mb-3.5">
+          <AlertDialog >
               <AlertDialogTrigger asChild>
-                <Button variant="destructive">Delete</Button>
+                <Button variant="destructive" className="bg-gradient-to-r from-rose-800 via-red-900 to-rose-950">Delete</Button>
               </AlertDialogTrigger>
               <AlertDialogContent>
                 <AlertDialogHeader>
@@ -111,12 +256,17 @@ export default function Account() {
                 </AlertDialogHeader>
                 <AlertDialogFooter>
                  
-                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogCancel className="bg-blue-500 border-transparent hover:bg-blue-800 duration-500 hover:text-white">Cancel</AlertDialogCancel>
                   <AlertDialogAction>Continue</AlertDialogAction>
                   
                 </AlertDialogFooter>
               </AlertDialogContent>
             </AlertDialog>
+            </div>
+          <Separator orientation="horizontal" className="bg-gray-300 mb-3.5" />
+            <CardFooter className="flex justify-center">
+            <div className="flex">
+            <Button className="bg-blue-500 border-transparent hover:bg-blue-800 duration-500 hover:text-white">Save Changes</Button>
             </div>
           </CardFooter>
         </Card>

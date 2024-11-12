edf75aee669f537c91174197a33ed86522e33920
Jack <jackmcgrath2k@gmail.com>
2024-11-12 16:47:16 +0000

    Journal Update

--- src/components/JournalGrid.jsx
+++ src/components/JournalGrid.jsx
@@ -1,6 +1,19 @@
 // components/JournalGrid.jsx
-import React from 'react';
-import {
+  import React from 'react';
+  import { Button } from "../components/ui/button";
+  import { Input } from "@/components/ui/input"
+  import { Label } from "@/components/ui/label"
+  import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+  import {
     Card,
     CardContent,
     CardDescription,
@@ -8,95 +21,134 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
-  import { Separator } from "@/components/ui/separator"
   import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-    TableFooter,
-  } from "@/components/ui/table"
-  
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  import { ScrollArea } from "@/components/ui/scroll-area"
+  import { Separator } from "@/components/ui/separator"
 
-  const invoices = [
-    {
-      invoice: "INV001",
-      paymentStatus: "Paid",
-      totalAmount: "$250.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV002",
-      paymentStatus: "Pending",
-      totalAmount: "$150.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV003",
-      paymentStatus: "Unpaid",
-      totalAmount: "$350.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV004",
-      paymentStatus: "Paid",
-      totalAmount: "$450.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV005",
-      paymentStatus: "Paid",
-      totalAmount: "$550.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV006",
-      paymentStatus: "Pending",
-      totalAmount: "$200.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV007",
-      paymentStatus: "Unpaid",
-      totalAmount: "$300.00",
-      paymentMethod: "Credit Card",
-    },
-  ]
+const tags = Array.from({ length: 50 }).map(
+  (_, i, a) => `v1.2.0-beta.${a.length - i}`
+)
+
+const ToggleBookmark = () => {
+  const [isSaved, setIsSaved] = useState(false);
+}
+  const handleToggle = () => {
+    setIsSaved((prev) => !prev);
+  };
 
 const JournalGrid = () => {
   return (
-    <div className='border-white m-24'>
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {invoices.map((invoice) => (
-          <TableRow key={invoice.invoice}>
-            <TableCell className="font-medium">{invoice.invoice}</TableCell>
-            <TableCell>{invoice.paymentStatus}</TableCell>
-            <TableCell>{invoice.paymentMethod}</TableCell>
-            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-          </TableRow>
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">Open</Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Entries</SheetTitle>
+          <SheetDescription>
+            Here are your journal entries.
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid gap-4 py-4">
+          <div className="items-center gap-4">
+          <ScrollArea className="h-1/6 w-full rounded-md border">
+      <div className="p-4">
+        <h4 className="mb-4 text-sm font-medium leading-none">Journal Entries</h4>
+        {tags.map((tag) => (
+          <>
+            <div key={tag} className="text-sm">
+            <div className='flex items-center justify-center'>
+  <div className=''>
+    <Card>
+      <CardHeader>
+       
+        <div className="flex justify-between items-center">
+         
+          <div className="flex items-center gap-4">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6 cursor-pointer"
+                
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+                />
+              </svg>
+            </div>
+            <div className="flex flex-col">
+              <CardTitle className="font-bold text-md">Entry #134</CardTitle>
+              <CardDescription className="text-xs">15 January 2025</CardDescription>
+            </div>
+          </div>
+
+          <div>
+          <DropdownMenu>
+              <DropdownMenuTrigger>
+              <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5 cursor-pointer"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>Entry #</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                <Button className="bg-blue-600 hover:bg-blue-700 duration-300">Add to Collection</Button>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                <Button className="bg-red-600 hover:bg-red-700 duration-300">Delete</Button>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </CardHeader>
+
+      <CardContent className="space-y-2">
+        <h1 className='text-xs text-gray-600'>Hover to reveal summary.</h1>
+        <div className="space-y-2 rounded bg-gray-300 text-gray-300 hover:bg-transparent hover:text-gray-800 duration-1000">
+          <h1 className="text-xs font-medium">
+            Summarized text content. Maybe use Vertex or Gemini or something else.
+          </h1>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
+</div>
+            </div>
+            <Separator className="my-2" />
+          </>
         ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">$2,500.00</TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
-    </div>
+      </div>
+    </ScrollArea>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
   )
 }
 
